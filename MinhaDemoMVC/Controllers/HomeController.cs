533e47af58f8592aa718e16460f9f69b3005b833
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-03-28 11:43:49 -0300

    first commit

---
+++ MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaDemoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")]
+    [Route("sales")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+       
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("list/{id:int}/{category:int?}")]
+        public IActionResult Index(int id, int category)
+        {
+            return View();
+        }
+
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            //return Json("{nome: Oseias}");
+            // var fileBytes = System.IO.File.ReadAllBytes(@"D:\Pedido.pdf");
+            //var fileName = "Pedido.pdf";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+            return Content("Teste");
+        }
+
+        [Route("error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
